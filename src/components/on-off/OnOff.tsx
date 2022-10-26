f85bb3937d11ee8f3d2ab485111fc1907d6c1d0d
yegorgursky <yahorhurski@gmail.com>
2022-10-26 18:37:52 +0300

    refactoring onOff component

--- src/components/on-off/OnOff.tsx
+++ src/components/on-off/OnOff.tsx
@@ -6,40 +6,42 @@ type OnOffPropsType = {
 
 export const OnOff = (props: OnOffPropsType) => {
 
+    const switchOnStyle = {
+        width: '80px',
+        height: '15px',
+        borderStyle: 'solid',
+        borderWidth: '3px',
+        borderRadius: '5px',
+        margin: '10px 0px 0px 10px',
+        display: 'inline-block',
+        background: props.switch ? 'green' : 'white'
+    };
+    const switchOffStyle = {
+        width: '80px',
+        height: '15px',
+        borderStyle: 'solid',
+        borderWidth: '3px',
+        borderRadius: '5px',
+        margin: '10px 0px 0px 10px',
+        display: 'inline-block',
+        background: props.switch ? 'white' : 'red'
+    };
+    const switchIndicatorStyle = {
+        width: '15px',
+        height: '15px',
+        borderStyle: 'solid',
+        borderWidth: '3px',
+        borderRadius: '5px',
+        margin: '10px 0px 0px 10px',
+        display: 'inline-block',
+        background: props.switch ? 'green' : 'red'
+    };
+
     return (
         <div>
-            <div style={{
-                width: '80px',
-                height: '15px',
-                borderStyle: 'solid',
-                borderWidth: '3px',
-                borderRadius: '5px',
-                margin: '10px 0px 0px 10px',
-                display: 'inline-block',
-                background: props.switch ? 'green' : 'white'
-            }}>On
-            </div>
-            <div style={{
-                width: '80px',
-                height: '15px',
-                borderStyle: 'solid',
-                borderWidth: '3px',
-                borderRadius: '5px',
-                margin: '10px 0px 0px 10px',
-                display: 'inline-block',
-                background: props.switch ? 'white' : 'red'
-            }}>Off
-            </div>
-            <div style={{
-                width: '15px',
-                height: '15px',
-                borderStyle: 'solid',
-                borderWidth: '3px',
-                borderRadius: '5px',
-                margin: '10px 0px 0px 10px',
-                display: 'inline-block',
-                background: props.switch ? 'green' : 'red'
-            }}></div>
+            <div style={switchOnStyle}>On</div>
+            <div style={switchOffStyle}>Off</div>
+            <div style={switchIndicatorStyle}></div>
         </div>
     );
 };
